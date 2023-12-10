2d9c70c60d2778c261c9ec7dda3da081e15bb2c3
WJK541 <15601036463@wo.cn>
2023-12-10 17:05:30 +0800

    project file

--- PrimeNumberCalculator.cpp
+++ PrimeNumberCalculator.cpp
@@ -9,17 +9,14 @@ double elapsed_time;
 bool restart = false;
 bool print_res = false;
 
-atomic<ull> cnt(0);
 atomic<ull> it(0);
-const ull th_count = std::thread::hardware_concurrency();
-const ull delta = th_count * 6 - 2;
+atomic<ull> cnt(0);
 mpz_t ZERO, ONE, TWO;
-
 
 void primelist(vector<ull> *primes, ull n, ull count)
 {
     const ull S = 524288;
-    ull result = 0, i, j, k, l, start, p;
+    ull i, j, k, l, start, p;
     Bitslist64 is_prime(S >> 1, true);
     for (k = (++it); k * S <= n; k = (++it)) {
         start = k * S;
@@ -31,23 +28,20 @@ void primelist(vector<ull> *primes, ull n, ull count)
         }
         for (i = 1; i < S and start + i <= n; i += 2) {
             if (is_prime.at(i >> 1)) {
-                result++;
-                // printf("%u ", i);
+                primes->push_back(i);
             }
         }
     }
-    cnt.store((ull)cnt + result);
 }
-void MTBEratosthenesSieve(ull n)
+vector<ull> MTBEratosthenesSieve(ull n)
 { // multithreaded blocked Eratosthenes sieve
     const ull S = 524288;
     vector<ull> primes;
-    ull nsqrt = (ull)sqrt(n), count = 0, result = 0, i, j, p;
+    ull nsqrt = (ull)sqrt(n), count = 0, i, j, p;
     Bitslist64 is_prime0((nsqrt >> 1) + 1, true);
     for (i = 3; i <= nsqrt; i += 2) {
         if (is_prime0.at(i >> 1)) {
             primes.push_back(i);
-            count++;
             for (j = i * i; j <= nsqrt; j += i << 1)
                 is_prime0.setfalse(j >> 1);
         }
@@ -60,91 +54,20 @@ void MTBEratosthenesSieve(ull n)
     }
     for (i = 3; i < S and i <= n; i += 2) {
         if (is_prime.at(i >> 1)) {
-            result++;
+            primes.push_back(i);
             // printf("%u ", i);
         }
     }
-    thread *threads = new thread[th_count];
+    vector<thread> threads;
     for (i = 0; i < th_count; i++)
-        threads[i] = thread(primelist, &primes, n, count);
+        threads.push_back(thread(primelist, &primes, n, count));
 
     for (i = 0; i < th_count; i++)
         threads[i].join();
 
-    result += (ull)cnt;
-    result++;
-    delete[] threads;
     cnt = 0;
     it = 0;
-}
-vector<ull> *PrimeList64(ull start, ull end)
-{
-    ull i, j, root = (ull)sqrt(end) + 1, count = 1;
-    Bitslist64 is_prime((end - 1) / 2, true);
-    start = max(3, start);
-    vector<ull> *primes;
-    primes->reserve((end / (log(end - 1.1))) - (start / (log(start - 1.1))));
-    for (i = 3; i < start; i += 2) {
-        if (is_prime.at(i >> 1)) {
-            for (j = i * i; j <= end; j += i << 1)
-                is_prime.setfalse(j >> 1);
-        }
-    }
-    for (i = start; i < root; i += 2) {
-        if (is_prime.at(i >> 1)) {
-            count++;
-            primes->push_back(i);
-            for (j = i * i; j <= end; j += i << 1)
-                is_prime.setfalse(j >> 1);
-        }
-    }
-    for (i = max(root, start) | 1; i <= end; i += 2) {
-        if (is_prime.at(i >> 1)) {
-            count++;
-            primes->push_back(i);
-        }
-    }
     return primes;
-}
-
-inline void output(int mode = 0)
-{
-    if (mode == 0) {
-        elapsed_time = duration_cast<milliseconds>(high_resolution_clock::now() - start_time).count() / 1000.0;
-        printf("The calculation takes %.4f seconds\n", elapsed_time);
-        return;
-    }
-    if (mode == 1) {
-        elapsed_time = duration_cast<microseconds>(high_resolution_clock::now() - start_time).count() / 1000.0;
-        printf("The calculation takes %.4f milliseconds\n", elapsed_time);
-    }
-}
-bool Miller_Rabin(ull n)
-{
-    ull u = n - 1, t = 0;
-    while (u % 2 == 0) {
-        u >>= 1;
-        t++;
-    }
-    for (ull a : millerrabin_prime) {
-        ull v = qpow(a, u, n);
-        if (v == 1 or v == n - 1 or v == 0)
-            continue;
-        for (ull j = 1; j <= t; j++) {
-            v = qmul(v, v, n);
-            if (v == n - 1 and j != t) {
-                v = 1;
-                break;
-            }
-            if (v == 1) {
-                return false;
-            }
-        }
-        if (v != 1) {
-            return false;
-        }
-    }
-    return true;
 }
 
 inline bool millerrabin(const mpz_t n, const mpz_t nm1, mpz_t exp, mpz_t p, uint t)
@@ -415,228 +338,6 @@ int strongPrimalityTest(ull n)
     return 0;
 }
 
-inline bool check_prime(ull n)
-{
-    if (n == 1) {
-        return false;
-    }
-    for (ull p : prime) {
-        if (n % p == 0)
-            return n == p;
-    }
-    ull u = n - 1, t = 0;
-    while (u % 2 == 0)
-        u /= 2, ++t;
-    // ull ud[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
-    for (ull a : millerrabin_prime) {
-        ull v = qpow(a, u, n);
-        if (v == 1 or v == n - 1 or v == 0)
-            continue;
-        for (ull j = 1; j < t; j++) {
-            v = qmul(v, v, n);
-            if (v == n - 1) {
-                v = 1;
-                break;
-            } // 出现一个n-1，后面都是1，直接跳出
-            if (v == 1)
-                return 0; // 这里代表前面没有出现n-1这个解，二次检验失败
-        }
-        v = qmul(v, v, n);
-        if (v == 1)
-            return 0;
-        if (v != 1)
-            return 0; // Fermat检验
-    }
-    return 1;
-}
-
-void a0(ull start, ull end, ull c)
-{
-    ull count = 0, num;
-    for (num = 6 * (start / 6 + c) + 5; num <= end;) {
-        if (check_prime(num)) {
-            count++;
-            printf("%llu ", num);
-        }
-        num += 2;
-
-        if (num > end)
-            break;
-
-        if (check_prime(num)) {
-            count++;
-            printf("%llu ", num);
-        }
-        num += delta;
-    }
-    cnt.store((ull)cnt + count);
-}
-void a1(ull start, ull end, ull c)
-{
-    ull count = 0, num;
-    for (num = 6 * (start / 6 + c) + 5; num <= end;) {
-        if (check_prime(num))
-            count++;
-        num += 2;
-
-        if (num > end)
-            break;
-
-        if (check_prime(num))
-            count++;
-        num += delta;
-    }
-    cnt.store((ull)cnt + count);
-}
-void primelist_multithread(ull start, ull end)
-{
-    if (start > end) {
-        printf("The START cannot be greater than the END!!!\n");
-        return;
-    }
-    printf("Within the range of [%llu, %llu], there are prime numbers:\n", start, end);
-    start_time = chrono::high_resolution_clock::now();
-    if (start <= 2 and 2 <= end) {
-        cnt++;
-    }
-    if (start <= 3) {
-        if (3 <= end)
-            cnt++;
-        else {
-            printf("\nThere are %llu prime numbers in total", (ull)cnt);
-            output();
-            return;
-        }
-    }
-    thread *threads = new thread[th_count];
-    ull i;
-    if (print_res) {
-        if (start % 6 == 0 or start % 6 == 1) {
-            if (start != 1 and check_prime(start)) {
-                printf("%llu ", start);
-                cnt++;
-            }
-        }
-        for (i = 0; i < th_count; i++)
-            threads[i] = thread(a0, start, end, i);
-    } else {
-        if (start % 6 == 0 or start % 6 == 1) {
-            if (start != 1 and check_prime(start))
-                cnt++;
-        }
-        for (i = 0; i < th_count; i++)
-            threads[i] = thread(a1, start, end, i);
-    }
-    for (i = 0; i < th_count; i++)
-        threads[i].join();
-
-    elapsed_time = duration_cast<milliseconds>(high_resolution_clock::now() - start_time).count() / 1000.0;
-    printf("\nThere are %llu prime numbers in total, and the calculation takes %.4f seconds\n", (ull)cnt, elapsed_time);
-    cnt = 0;
-    delete[] threads;
-}
-
-void factorize(ull n)
-{
-    if (n == 1) {
-        printf("Invalid input!!!\n");
-        return;
-    }
-    ull root = (ull)sqrt(n), count = 0, types = 0, indices = 1, i = 7;
-    vector<ull> factors;
-    printf("Factors         Exponent\n");
-    start_time = chrono::high_resolution_clock::now();
-    while (n != 1 && 2 <= root) {
-        if (n % 2 == 0) {
-            n = n / 2;
-            factors.push_back(2);
-            count++;
-        } else
-            break;
-    }
-    while (n != 1 && 3 <= root) {
-        if (n % 3 == 0) {
-            n = n / 3;
-            factors.push_back(3);
-            count++;
-        } else
-            break;
-    }
-    while (n != 1 && 5 <= root) {
-        if (n % 5 == 0) {
-            n = n / 5;
-            factors.push_back(5);
-            count++;
-        } else
-            break;
-    }
-    while (n != 1 && i <= root) {
-        if (n % i == 0) {
-            n = n / i;
-            factors.push_back(i);
-            count++;
-        } else
-            i += 4; // 7+4=11
-        if (n % i == 0) {
-            n = n / i;
-            factors.push_back(i);
-            count++;
-        } else
-            i += 2; // 11+2=13
-        if (n % i == 0) {
-            n = n / i;
-            factors.push_back(i);
-            count++;
-        } else
-            i += 4; // 13+4=17
-        if (n % i == 0) {
-            n = n / i;
-            factors.push_back(i);
-            count++;
-        } else
-            i += 2; // 17+2=19
-        if (n % i == 0) {
-            n = n / i;
-            factors.push_back(i);
-            count++;
-        } else
-            i += 4; // 19+4=23
-        if (n % i == 0) {
-            n = n / i;
-            factors.push_back(i);
-            count++;
-        } else
-            i += 6; // 23+6=29
-        if (n % i == 0) {
-            n = n / i;
-            factors.push_back(i);
-            count++;
-        } else
-            i += 2; // 29+2=31
-        if (n % i == 0) {
-            n = n / i;
-            factors.push_back(i);
-            count++;
-        } else
-            i += 6; // 31+6=37
-    }
-    if (n != 1) {
-        factors.push_back(n);
-        count++;
-    }
-    factors.push_back(0);
-    for (i = 0; i < count; i++) {
-        if (factors[i] != factors[(ull)i + 1]) {
-            cout << factors[i] << "		" << indices << endl;
-            indices = 1;
-            types++;
-        } else
-            indices++;
-    }
-    elapsed_time = duration_cast<microseconds>(high_resolution_clock::now() - start_time).count() / 1000.0;
-    printf("The calculation takes %.4f milliseconds\n", elapsed_time);
-}
-
 bool execute_command(string command)
 {
     if (command == "/m") {
@@ -772,18 +473,20 @@ int main()
                 if (restart)
                     break; // 重新选择
 
-                PrimeList64(arg1, arg2);
+                for (ull p : PrimeList64(arg1, arg2)) {
+                    cout << p << ' ';
+                }
+                cout << "\nFinish\n";
                 cout << choose_mode_info;
                 break;
 
             case '2': // 模式2，判断一个数是否为质数
+                      // 计划删除
                 printf("[Current mode]: Primality test\n");
                 arg1 = get_argument("Please enter a positive integer: ");
                 if (restart)
                     break; // 重新选择
-                start_time = high_resolution_clock::now();
-                t1 = check_prime(arg1);
-                output(1);
+                t1 = MillerRabin(arg1);
                 if (t1)
                     printf("YES\n");
 
@@ -799,7 +502,10 @@ int main()
                 if (restart)
                     break;
 
-                factorize(arg1);
+                for (auto i : PrimeFactorization(arg1)) {
+                    cout << (i.first) << "   " << (i.second) << '\n';
+                }
+                cout << "\nFinish\n";
                 cout << choose_mode_info;
                 break;
 
@@ -813,7 +519,10 @@ int main()
                 if (restart)
                     break; // 重新选择
 
-                primelist_multithread(arg1, arg2);
+                for (auto i : PrimeListMT(arg1, arg2)) {
+                    cout << i << ' ';
+                }
+                cout << "\nFinish\n";
                 cout << choose_mode_info;
                 break;
 
@@ -823,8 +532,10 @@ int main()
                 if (restart)
                     break; // 重新选择
 
-                MTBEratosthenesSieve(arg1);
-
+                for (ull i : MTBEratosthenesSieve(arg1)) {
+                    cout << i << ' ';
+                }
+                cout << "\nFinish\n";
                 cout << choose_mode_info;
                 break;
 
@@ -834,7 +545,11 @@ int main()
                 if (restart)
                     break; // 重新选择
 
-                Miller_Rabin(arg1);
+                t1 = MillerRabin(arg1);
+                if (t1)
+                    cout << "YES";
+                else
+                    cout << "NO";
 
                 cout << choose_mode_info;
                 break;
@@ -866,7 +581,6 @@ int main()
                     break; // 重新选择
                 start_time = high_resolution_clock::now();
                 t1 = lucas(arg1);
-                output(1);
                 if (t1)
                     printf("YES\n");
                 else
--- head.h
+++ head.h
@@ -14,8 +14,14 @@
 #define max(x, y) (((x) > (y)) ? (x) : (y))
 #define min(x, y) (((x) < (y)) ? (x) : (y))
 #define qmul(x, y, mod) (((__int128)x * y) % mod)
-// #define START_TIMING auto start_time = chrono::steady_clock::now();
-// #define END_TIMING
+#define pi_n(n) (n / (log(n) - 1.1))
+#define START_TIMING auto start_time = chrono::steady_clock::now();
+#define END_TIMING_S                                                                                      \
+    elapsed_time = duration_cast<miliseconds>(chrono::steady_clock::now() - start_time).count() / 1000.0; \
+    printf("The calculation takes %.4f seconds\n", elapsed_time);
+#define END_TIMING_MS                                                                                      \
+    elapsed_time = duration_cast<microseconds>(chrono::steady_clock::now() - start_time).count() / 1000.0; \
+    printf("The calculation takes %.4f miliseconds\n", elapsed_time);
 
 using namespace std;
 using namespace chrono;
@@ -28,6 +34,7 @@ typedef long double ld;
 
 const ull millerrabin_prime[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022};
 const ull prime[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+const ull th_count = std::thread::hardware_concurrency();
 
 std::map<std::string, const std::string> test_set = {
     {"test1000", "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111991"},
@@ -105,6 +112,38 @@ void Bitslist64::reset(bool a)
     }
 }
 
+class Range
+{
+public:
+    atomic<ull> var;
+    atomic<bool> flag = true;
+    ull up;
+    Range(ull start, ull end)
+    {
+        if (start > end)
+            throw "START can not be greater than END!";
+        ull r = start % 6;
+        if (r == 0 or r == 1) {
+            start = ((start / 6) * 6) + 1;
+            flag = false;
+        } else {
+            flag = true;
+            start = ((start / 6) * 6) + 5;
+        }
+        var.store(start);
+        up = end;
+    }
+    ull get()
+    {
+        if (flag) {
+            flag = false;
+            return var.fetch_add(2);
+        }
+        flag = true;
+        return var.fetch_add(4);
+    }
+};
+
 inline ull qpow(ull base, ull exp, ull mod)
 {
     ull res = 1;
@@ -135,7 +174,7 @@ inline ll absmod(ll x, ll mod)
     return x + mod;
 }
 
-bool TrialAndError(ull n)
+bool TrialAndError(ull n, const ull *P = prime)
 {
     /*
     this function can only test Pseudoprime Number
@@ -149,6 +188,127 @@ bool TrialAndError(ull n)
 
 bool MillerRabin(ull n)
 {
-    ;
+    ull nm1 = n - 1; // n minus 1
+    ull t = 0, s = nm1;
+    while (s & 1 == 0) {
+        s >>= 1;
+        t++;
+    }
+    // s * 2 ^ t = nm1
+    ull v;
+    for (ull p : millerrabin_prime) {
+        v = qpow(p, s, n);
+        if (v == 0 or v == 1 or v == nm1)
+            continue;
+        for (ull j = 1; j < t; j++) {
+            v = qmul(v, v, n);
+            if (v == nm1) {
+                // v = 1;
+                // break;
+                goto NEXT_LOOP;
+            }
+            if (v == 1)
+                return false;
+        }
+        if (v != 1)
+            return false;
+    NEXT_LOOP:;
+    }
     return true;
+}
+
+map<ull, ull> PrimeFactorization(ull n)
+{
+    ull root = (ull)sqrt(n), exp = 0, i = 5;
+    map<ull, ull> result = {};
+
+#define factorize(i)     \
+    while (n % i == 0) { \
+        n /= i;          \
+        exp++;           \
+    }                    \
+    if (exp) {           \
+        result[i] = exp; \
+        exp = 0;         \
+    }                    \
+    if (n == 1)          \
+        return result;
+
+    factorize(2);
+    factorize(3);
+    while (i <= root) {
+        factorize(i);
+        i += 2;
+        factorize(i);
+        i += 4;
+    }
+    if (n > 1) {
+        result[n] = 1;
+    }
+    return result;
+}
+
+vector<ull> PrimeList64(ull start, ull end)
+{
+    ull i, j, root = (ull)sqrt(end) + 1;
+    Bitslist64 is_prime((end - 1) / 2, true);
+    start = max(3, start);
+    vector<ull> primes;
+    primes.reserve(pi_n(end) - pi_n(start));
+    if (start <= 2 and 2 <= end)
+        primes.push_back(2);
+    for (i = 3; i < start; i += 2) {
+        if (is_prime.at(i >> 1)) {
+            for (j = i * i; j <= end; j += i << 1)
+                is_prime.setfalse(j >> 1);
+        }
+    }
+    for (i = start; i < root; i += 2) {
+        if (is_prime.at(i >> 1)) {
+            primes.push_back(i);
+            for (j = i * i; j <= end; j += i << 1)
+                is_prime.setfalse(j >> 1);
+        }
+    }
+    for (i = max(root, start) | 1; i <= end; i += 2) {
+        if (is_prime.at(i >> 1)) {
+            primes.push_back(i);
+        }
+    }
+    return primes;
+}
+
+void PrimeListMT_Impl(ull end, vector<ull> *primes, Range *range)
+{
+    ull num;
+    for (;;) {
+        num = range->get();
+        if (num > end)
+            return;
+        for (ull i = 2; i < 25; i++) {
+            if (num % prime[i] == 0) {
+                if (num == prime[i])
+                    primes->push_back(num);
+                goto NEXT_LOOP;
+            }
+        }
+        if (MillerRabin(num))
+            primes->push_back(num);
+    NEXT_LOOP:;
+    }
+}
+vector<ull> PrimeListMT(ull start, ull end)
+{ // PrimeList_Multithreaded
+    vector<ull> primes;
+    if (start <= 2 and 2 <= end) {
+        primes.push_back(2);
+    }
+    start = max(3, start);
+    Range range(start, end);
+    vector<thread> threads;
+    for (ull i = 0; i < th_count; i++)
+        threads.push_back(thread(PrimeListMT_Impl, end, &primes, &range));
+    for (ull i = 0; i < th_count; i++)
+        threads[i].join();
+    return primes;
 }
